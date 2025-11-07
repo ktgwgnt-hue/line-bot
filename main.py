ef7b6606e872f98898e982f4ad020fbfe442f8cc
ktgwgnt-hue <ktgwgnt@gmail.com>
2025-11-08 02:52:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+from flask import Flask, request
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+app = Flask(__name__)
+
+LINE_CHANNEL_ACCESS_TOKEN = "ここにチャンネルアクセストークン"
+LINE_CHANNEL_SECRET = "ここにチャンネルシークレット"
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        return "Invalid signature", 400
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="返信テスト成功！")
+    )
+
+if __name__ == "__main__":
+    app.run()
 
